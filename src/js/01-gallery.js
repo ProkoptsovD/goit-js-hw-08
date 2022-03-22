68b682e487d9d5494917b09deb71cc352aa41c6f
ProkoptsovD <dimka.prokoptsov@gmail.com>
2022-03-22 18:00:25 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,29 @@
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const galleryCardsMarkup = makeGalleryCardsMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', galleryCardsMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  overlayOpacity: 0.8,
+  fadeSpeed: 200,
+  maxZoom: 2,
+  scrollZoomFactor: 0.2,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function makeGalleryCardsMarkup(images) {
+  return images.map(({ description, original, preview }) => {
+    return `
+                    <a class="gallery__item" href="${original}">
+                        <img class="gallery__image" src="${preview}" alt="${description}" />
+                    </a>`;
+  }).join``;
+}
