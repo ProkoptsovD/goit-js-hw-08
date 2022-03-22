b5c19bac2146c855040b05e7edb6f70385fd7102
ProkoptsovD <dimka.prokoptsov@gmail.com>
2022-03-22 21:53:32 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
+const throttle = require('lodash.throttle');
 
+const feedbackFormElem = document.querySelector('.feedback-form');
+
+feedbackFormElem.addEventListener('input', onFeedbackFormSubmit);
+
+function onFeedbackFormSubmit(event) {
+  event.preventDefault();
+
+  const INPUT = 'INPUT';
+  const TEXTAREA = 'TEXTAREA';
+  const currentElement = event.target.nodeName;
+
+  if (currentElement === INPUT) {
+  }
+
+  if (currentElement === TEXTAREA) {
+  }
+  const userFeedback = event.target;
+  const { email, message } = event.target;
+
+  console.dir(event.target);
+}
