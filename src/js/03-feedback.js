434c55dc5bd39b5f6d935573d5c71830d2e270bc
ProkoptsovD <dimka.prokoptsov@gmail.com>
2022-03-23 13:37:56 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,23 +1,42 @@
 const throttle = require('lodash.throttle');
 
+const LOCALSTORAGE_FEEDBACK_KEY = 'feedback-form-state';
+const FEEDBACK_DATA = {};
+
 const feedbackFormElem = document.querySelector('.feedback-form');
 
-feedbackFormElem.addEventListener('input', onFeedbackFormSubmit);
+window.addEventListener('load', onWindowLoad);
+feedbackFormElem.addEventListener('input', throttle(onFeedbackFormInput, 500));
+feedbackFormElem.addEventListener('submit', onFeedbackFormSibmit);
 
-function onFeedbackFormSubmit(event) {
+function onFeedbackFormInput(event) {
+  const { name, value } = event.target;
+
+  FEEDBACK_DATA[name] = value;
+  localStorage.setItem(LOCALSTORAGE_FEEDBACK_KEY, JSON.stringify(FEEDBACK_DATA));
+}
+
+function onWindowLoad() {
+  try {
+    const formDataObj = JSON.parse(localStorage.getItem(LOCALSTORAGE_FEEDBACK_KEY));
+    const formInput = document.querySelector('input[name="email"]');
+    const formTextarea = document.querySelector('textarea[name="message"]');
+
+    formInput.value = formDataObj.email;
+    formTextarea.value = formDataObj.message;
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+}
+
+function onFeedbackFormSibmit(event) {
   event.preventDefault();
 
-  const INPUT = 'INPUT';
-  const TEXTAREA = 'TEXTAREA';
-  const currentElement = event.target.nodeName;
+  const feedbackForm = event.currentTarget;
+  const formDataObj = JSON.parse(localStorage.getItem(LOCALSTORAGE_FEEDBACK_KEY));
 
-  if (currentElement === INPUT) {
-  }
-
-  if (currentElement === TEXTAREA) {
-  }
-  const userFeedback = event.target;
-  const { email, message } = event.target;
-
-  console.dir(event.target);
+  console.log(formDataObj);
+  localStorage.removeItem(LOCALSTORAGE_FEEDBACK_KEY);
+  feedbackForm.reset();
 }
